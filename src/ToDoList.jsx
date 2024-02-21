918ab7e093a03e7daec974b50aeb7fe27ec0f646
Arshdeep Kaur <sainiarshdeep114@gmail.com>
2024-02-21 15:17:24 +0530

    New Changes

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,15 +1,73 @@
 import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 export default function ToDoList() {
-  let [todos, setToDos] = useState([{ task: "Code", id: uuidv4() }]);
+  let styles = { textDecoration: "line-through" };
+
+  let [todos, setToDos] = useState([
+    { task: "Code", id: uuidv4(), isDone: false },
+  ]);
   let [newToDo, setNewToDos] = useState([""]);
 
+  //Adding Task
   function addTask() {
-    setToDos([...todos, { task: newToDo, id: uuidv4() }]);
+    setToDos((prevToDos) => {
+      return [...prevToDos, { task: newToDo, id: uuidv4(), isDone: false }];
+    });
     setNewToDos("");
   }
+
+  //Deleting Task
+  function deleteToDO(id) {
+    setToDos((prevToDos) => {
+      return prevToDos.filter((todo) => todo.id != id);
+    });
+  }
+
+  //Setting Task Value
   let updateToDoValue = (event) => {
     setNewToDos(event.target.value);
+  };
+  //Updating Functionality
+  let upperCaseAll = () => {
+    setToDos((prevToDos) =>
+      prevToDos.map((todo) => {
+        return { ...todo, task: todo.task.toUpperCase() };
+      })
+    );
+  };
+
+  //Updating one element
+  let upperCaseOne = (id) => {
+    setToDos((prevToDos) =>
+      prevToDos.map((todo) => {
+        if (todo.id == id) {
+          return { ...todo, task: todo.task.toUpperCase() };
+        } else {
+          return { ...todo };
+        }
+      })
+    );
+  };
+
+  //Is task Done
+  let isTaskDone = (id) => {
+    setToDos((prevToDos) =>
+      prevToDos.map((todo) => {
+        if (todo.id == id) {
+          return { ...todo, isDone: true };
+        } else {
+          return { ...todo };
+        }
+      })
+    );
+  };
+  //All Done
+  let isAllDone = () => {
+    setToDos((prevToDos) =>
+      prevToDos.map((todo) => {
+        return { ...todo, isDone: true };
+      })
+    );
   };
   return (
     <div>
@@ -27,9 +85,22 @@ export default function ToDoList() {
       <h4>Tasks To-Do</h4>
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.task}</li>
+          <li key={todo.id}>
+            {todo.isDone ? (
+              <span style={styles}>{todo.task}</span>
+            ) : (
+              <span>{todo.task}</span>
+            )}
+            &nbsp; &nbsp; &nbsp;
+            <button onClick={() => deleteToDO(todo.id)}>Delete</button>
+            &nbsp; &nbsp; &nbsp;
+            <button onClick={() => isTaskDone(todo.id)}>Mark as Done</button>
+          </li>
         ))}
       </ul>
+      <br />
+      <br />
+      <button onClick={isAllDone}>All Done</button>
     </div>
   );
 }
